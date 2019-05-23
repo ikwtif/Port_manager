60171f79040981c038c1d7b71e6d2567b5a9a49c
ikwtif <dieter.debruyne@gmail.com>
2019-05-23 20:44:12 +0200

    initial commit

---
+++ api/openmarketcap.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def get_data():
+    response = requests.get("http://api.openmarketcap.com/api/v1/tokens")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+
+if __name__ == '__main__':
+    get_data()
---
+++ routes.py
@@ -0,0 +1,24 @@
+from flask import render_template
+from app import app
+import matplotlib.pyplot as plt
+import io
+import base64
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')
+
+@app.route('/graph')
+def graph(chartID = 'chart_ID', chart_type = 'line', chart_height = 500):
+    chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
+    series = [{"name": 'Label1', "data": [1, 2, 3]}, {"name": 'Label2', "data": [4, 5, 6]}]
+    title = {"text": 'My Title'}
+    xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
+    yAxis = {"title": {"text": 'yAxis Label'}}
+    return render_template('page2.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis,
+                           yAxis=yAxis)
