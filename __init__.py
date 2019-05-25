9614f80174480d670fe55d1701ffb30eea7b27db
ikwtif <dieter.debruyne@gmail.com>
2019-05-25 13:23:38 +0200

    initial commit

---
+++ __init__.py
@@ -0,0 +1,48 @@
+from flask import render_template
+from app import app
+
+
+@app.route('/')
+def homepage():
+
+    title = "Epic Tutorials"
+    paragraph = ["wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!","wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!"]
+
+    try:
+        return render_template("index.html", title = title, paragraph=paragraph)
+    except Exception as e:
+        return str(e)
+
+@app.route('/about')
+def aboutpage():
+
+    title = "About this site"
+    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
+
+    pageType = 'about'
+
+    return render_template("index.html", title=title, paragraph=paragraph, pageType=pageType)
+
+
+@app.route('/about/contact')
+def contactPage():
+
+    title = "About this site"
+    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
+
+    pageType = 'about'
+
+    return render_template("index.html", title=title, paragraph=paragraph, pageType=pageType)
+
+
+
+@app.route('/graph')
+def graph(chartID = 'chart_ID', chart_type = 'line', chart_height = 500):
+	chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
+	series = [{"name": 'Label1', "data": [1,2,3]}, {"name": 'Label2', "data": [4, 5, 6]}]
+	title = {"text": 'My Title'}
+	xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
+	yAxis = {"title": {"text": 'yAxis Label'}}
+	return render_template('graph.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
+ 
+
