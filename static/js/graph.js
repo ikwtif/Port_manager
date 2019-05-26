6d36f7afcf92049f3888f16097c1755b3f9e97dc
ikwtif <dieter.debruyne@gmail.com>
2019-05-26 13:13:46 +0200

    2 charts on page

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,10 +1,20 @@
   
 $(document).ready(function() {
-	$(chart_id).highcharts({
-		chart: chart,
-		title: title,
-		series: series,
-		tooltip: tooltip,
-		plotOptions: plotOptions
+	$(chart_id1).highcharts({
+		chart: chart1,
+		title: title1,
+		series: series1,
+		tooltip: tooltip1,
+		plotOptions: plotOptions1
+	});
+});
+
+$(document).ready(function() {
+	$(chart_id2).highcharts({
+		chart: chart2,
+		title: title2,
+		series: series2,
+		tooltip: tooltip2,
+		plotOptions: plotOptions2
 	});
 });
