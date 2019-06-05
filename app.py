83fc261116b29bc05ad80e7ad74fa9026a5b6e47
ikwtif <dieter.debruyne@gmail.com>
2019-06-05 20:45:35 +0200

    fix app setting

--- app.py
+++ app.py
@@ -21,6 +21,6 @@ from __init__ import *
 if __name__ == "__main__":
 
     if settings_main['app']:
-        app.run(debug=True, host='0.0.0.0', port=8080, passthrough_errors=True)
-    else:
         init_gui(app, width=1000, height=1000)
+    else:
+        app.run(debug=True, host='0.0.0.0', port=8080, passthrough_errors=True)
